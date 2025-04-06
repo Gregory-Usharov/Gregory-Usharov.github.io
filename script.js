691b1778f38c7fef8aba0da2c734f12d81cf49ac
Gregory-Usharov <gregory.usharov24@ycdsbk12.ca>
2025-04-06 01:27:40 -0400

    cleaned up code and removed test words from page, also added some performance improving code!

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 const background = document.getElementById('background');
-
-/*document.addEventListener('mousemove', (event) => {
-  //need to use mouth calculated things!!!
-    const xPercent = 205 - (event.clientX / window.innerWidth) * 30;
-    const yPercent = 205 - (event.clientY / window.innerHeight) * 30;
-    background.animate(
-      {
-         backgroundPosition: `${xPercent}% ${yPercent}%`
-      },
-      {
-        duration: 1200
-      }
-
-    );
-  });
-*/
 let lastMove = 0;
 //for maintaining performance
 let targetX = 50;
 let targetY = 50;
 let currentX = 50;
 let currentY = 50;
-
+//for following the mouse!
 document.addEventListener('mousemove', (event) => {
   //ensures the function runs less to better performance :)
   const now = Date.now();
@@ -29,6 +13,7 @@ document.addEventListener('mousemove', (event) => {
   lastMove = now;
   targetX = 50 - ((event.clientX / window.innerWidth) - 0.5) * 30;
   targetY = 50 - ((event.clientY / window.innerHeight) - 0.5) * 30;
+  //make this based off of window size rather than just random variables lol
 });
 
   function animateBackground() {
