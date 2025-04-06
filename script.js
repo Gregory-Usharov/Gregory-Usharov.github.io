440da3ecbf84eb64aa9643b05bd6137e982485e8
Gregory-Usharov <gregory.usharov24@ycdsbk12.ca>
2025-04-06 00:02:22 -0400

    added basic mouse movement to background

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-const r = document.querySelector('body');
-
-// Create a function for getting a vardiable value
-function myFunction_get() {
-  // Get the styles (properties and values) for the root
-  var rs = getComputedStyle(r);
-  // Alert the value of the --blue variable
-  alert("The value of --blue is: " + rs.getPropertyValue('--sizex'));
-}
-
-// Create a function for setting a variable value
-function myFunction_set() {
-  // Set the value of variable --blue to another value (in this case "lightblue")
-  r.style.setProperty('--sizex', '5%');
-}
+const background = document.getElementById('background');
 
 document.addEventListener('mousemove', (event) => {
-    const xPercent = (event.clientX / window.innerWidth) * 100;
-    const yPercent = (event.clientY / window.innerHeight) * 100;
+  //need to use mouth calculated things!!!
+    const xPercent = 205 - (event.clientX / window.innerWidth) *  30;
+    const yPercent = 205 - (event.clientY / window.innerHeight) * 30;
   
     // Set CSS variables with percent values
-    document.documentElement.style.setProperty('--sizex', `${xPercent}%`);
-    document.documentElement.style.setProperty('--sizey', `${yPercent}%`);
+    //background.
+    document.documentElement.style.setProperty('--xpos', `${xPercent}%`);
+    document.documentElement.style.setProperty('--ypos', `${yPercent}%`);
   });
 
